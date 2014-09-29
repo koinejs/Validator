ac868be3e560434389930e580b6e8e38b7b9d845
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-09-29 14:53:56 -0300

    Working code

--- specs/koine-validator_spec.js
+++ specs/koine-validator_spec.js
@@ -1,19 +1,22 @@
-"use strict";
 
 var Koine = require('../src/koine-validator.js');
 
-var UserValidator = Koine.Validator.extend({
-    processValidation: function (value) {
+var UserValidator = function () {};
 
+UserValidator.prototype = Koine.Validator.prototype;
+
+UserValidator.prototype.processValidation = function (user) {
+    if (!user.name) {
+        this.getErrors().push('Invalid');
     }
-});
+};
 
 describe("Koine.Validator", function () {
     var validator;
     var user;
 
     beforeEach(function () {
-        validator = Koine;
+        validator = new UserValidator();
         user = null;
     });
 
@@ -22,8 +25,46 @@ describe("Koine.Validator", function () {
         expect(validator).not.toBeUndefined(null);
     });
 
-    it("returns false when object is not valid", function () {
+    it("returns false when object is not valid but true when it is", function () {
         user = {};
         expect(validator.isValid(user)).toBeFalsy();
+
+        user.name = "John Doe";
+        expect(validator.isValid(user)).toBeTruthy();
+    });
+});
+
+describe("Koine.Validator.Errors", function () {
+    var errors;
+
+    beforeEach(function () {
+        errors = new Koine.Validator.Errors();
+    });
+
+    it("is initially empty", function () {
+        expect(errors.isEmpty()).toBeTruthy();
+    });
+
+    it("isEmpty() returns either true or false", function () {
+        expect(errors.isEmpty()).toBeTruthy();
+        errors.add("field", "error");
+        expect(errors.isEmpty()).toBeFalsy();
+    });
+
+    it("can add errors to the object", function () {
+        errors.add("field", "message")
+            .add("field", "message 2")
+            .add("field2", "message");
+
+        expect(errors.toJson()).toEqual({
+            field: ["message", "message 2"],
+            field2: ["message"]
+        });
+    });
+
+    it("can empty error collection", function () {
+        errors.add("some", "some message");
+
+        expect(errors.clear().isEmpty()).toBeTruthy();
     });
 });
--- src/koine-validator.js
+++ src/koine-validator.js
@@ -1,18 +1,94 @@
-"use strinct";
-
 (function (Koine) {
-    Koine.Validator = {
-        extend: function (methods) {
-            for (var method in methods) {
-                this[method] = methods[method];
-            }
-            return this;
-        },
-        isValid: function (value) {
-        },
-        processValidation: function (value) {
+    "use strinct";
 
+    /**
+     * Errors Validator
+     * Responsible for abstracting some validations
+     */
+    Koine.Validator = function () {
+        this._errors = {};
+    };
+
+    /**
+     * Process validation
+     */
+    Koine.Validator.prototype.processValidation = function (value) {
+        throw new Error("Not inplemented");
+    };
+
+    /**
+     * Process validation
+     * @param mixed value
+     * @return boolean
+     */
+    Koine.Validator.prototype.isValid =  function (value) {
+        this._errors = [];
+        this.processValidation(value);
+
+        return this.getErrors().length === 0;
+    };
+
+    /**
+     * Get the collection of errors
+     * @return Koine.Validator.Errors
+     */
+    Koine.Validator.prototype.getErrors = function () {
+        this._errors = this._errors || [];
+
+        return this._errors;
+    }
+
+    /**
+     * Errors Class
+     * Responsible for keeping track of errors
+     */
+    Koine.Validator.Errors = function () {
+        this._errors = {};
+    };
+
+    /**
+     * Verifies if the error collection is empty
+     * @return boolean
+     */
+    Koine.Validator.Errors.prototype.isEmpty = function () {
+        for (var prop in this._errors) {
+            return false;
         }
+
+        return true;
+    };
+
+    /**
+     * Add error by key
+     * @param string key
+     * @param string message
+     * @return self
+     */
+    Koine.Validator.Errors.prototype.add = function (key, message) {
+        this._errors[key] = this._errors[key] || [];
+        this._errors[key].push(message);
+
+        return this;
+    };
+
+    /**
+     * Converts errors to json
+     * @return json
+     */
+    Koine.Validator.Errors.prototype.toJson = function () {
+        return this._errors;
+    };
+
+    /**
+     * Clear errors
+     * @return boolean
+     */
+    Koine.Validator.Errors.prototype.clear = function () {
+        for(var prop in this._errors) {
+            delete this._errors[prop];
+        }
+
+        return this;
     };
 
 })(typeof(exports) === "undefined" ? (this.Koine || (this.Koine = {})) : exports);
